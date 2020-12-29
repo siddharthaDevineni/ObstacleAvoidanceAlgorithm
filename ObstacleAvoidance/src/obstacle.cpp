fc16d0a44fbd9b6702b33903f3fadeaee3edac07
kartikkanugo <karthik.kanugo@gmail.com>
2020-12-29 08:33:08 +0100

    setup to vscode complete

---
+++ ObstacleAvoidance/src/obstacle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "obstacleAvoidanceLib.h"
+
+int main()
+{
+	// get a value from the user
+	std::cout << "Enter an integer: ";
+	int num{};
+	std::cin >> num;
+
+	// print the value doubled
+	std::cout << num << " doubled is: " << num * 2 << " "<< add(5,3)<< '\n';
+
+	std::cout << "systempaused";
+	int num22{};
+	std::cin >> num22;
+
+	return 0;
+}
---
+++ ObstacleAvoidance/src/obstacleAvoidanceLib.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//Add Libs below
+
+int add(int a, int b) {
+	return a + b;
+}
