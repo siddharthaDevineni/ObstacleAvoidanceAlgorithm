03dcb81962723bd50450554bc6127abb908db6ce
kartikkanugo <karthik.kanugo@gmail.com>
2021-03-07 23:12:48 +0100

    prettier changes

--- ObstacleAvoidance/src/functionsRepandAtt.cpp
+++ ObstacleAvoidance/src/functionsRepandAtt.cpp
@@ -1,19 +1,16 @@
-#include<iostream>
-#include<math.h>
-#include<algorithm>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
 #include "functionsRepandAtt.h"
 
-
-
-
-o_errt Forces::forceAtt(float x, float y, float xg, float yg, Oresult* out)
+o_errt Forces::forceAtt(float x, float y, float xg, float yg, Oresult *out)
 {
     int ka;
-    float Fax,Fay,Fa;
-    Fax = -ka*(x - xg); // X-component of Fatt : Fa_x
-    Fay = -ka*(y - yg); // Y-component of Fatt : Fa_y
+    float Fax, Fay, Fa;
+    Fax = -ka * (x - xg); // X-component of Fatt : Fa_x
+    Fay = -ka * (y - yg); // Y-component of Fatt : Fa_y
 
-    Fa = sqrt(pow(Fax,2)+pow(Fay,2)); //Total attraction force Fa
+    Fa = sqrt(pow(Fax, 2) + pow(Fay, 2)); //Total attraction force Fa
 
     out->oResultF = Fa;
     out->oError = o_errt::err_no_error;
@@ -21,32 +18,31 @@ o_errt Forces::forceAtt(float x, float y, float xg, float yg, Oresult* out)
     return o_errt::err_no_error;
 }
 
-o_errt Forces::forceRep(float x, float y, float xo, float yo, Oresult* out)
+o_errt Forces::forceRep(float x, float y, float xo, float yo, Oresult *out)
 {
-    int kr,ro;
+    int kr, ro;
     float p, Frx, Fry, Fr;
-    p = sqrt(pow((x-xo),2)+pow((y-yo),2));
-    if(p<=ro)
-            {
-                Frx = kr*(pow(p,-1)-pow(ro,-1))*(pow(p,-2))*(x-xo)*pow(abs(x-xo),-1);
-            }
+    p = sqrt(pow((x - xo), 2) + pow((y - yo), 2));
+    if (p <= ro)
+    {
+        Frx = kr * (pow(p, -1) - pow(ro, -1)) * (pow(p, -2)) * (x - xo) * pow(abs(x - xo), -1);
+    }
     else
-        {
-            Frx = 0;
-        }
-    if(p<=ro)
-            {
-                Fry = kr*(pow(p,-1)-pow(ro,-1))*(pow(p,-2))*(y-yo)*pow(abs(y-yo),-1);
-            }
+    {
+        Frx = 0;
+    }
+    if (p <= ro)
+    {
+        Fry = kr * (pow(p, -1) - pow(ro, -1)) * (pow(p, -2)) * (y - yo) * pow(abs(y - yo), -1);
+    }
     else
-        {
-            Fry = 0;
-        }
-    Fr = sqrt(pow(Frx,2)+pow(Fry,2)); // Total repulsiom force Fr
+    {
+        Fry = 0;
+    }
+    Fr = sqrt(pow(Frx, 2) + pow(Fry, 2)); // Total repulsiom force Fr
 
     out->oResultF = Fr;
     out->oError = o_errt::err_no_error;
 
     return o_errt::err_no_error;
 }
-
