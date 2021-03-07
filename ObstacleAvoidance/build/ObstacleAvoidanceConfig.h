a019a140ae77b0059b2be7eeb00f89162e2c0682
kartikkanugo <karthik.kanugo@gmail.com>
2021-03-07 12:13:20 +0100

    updated versioning of obstacle avoidance

---
+++ ObstacleAvoidance/build/ObstacleAvoidanceConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Obstacle Avoidance
+#define OBSTACLEAVOIDANCE_VERSION_MAJOR 1
+#define OBSTACLEAVOIDANCE_VERSION_MINOR 0
--- ObstacleAvoidance/main.cpp
+++ ObstacleAvoidance/main.cpp
@@ -2,16 +2,18 @@
 #include "functionsRepandAtt.h"
 #include "obstacleAvoidance.h"
 #include "matplotlibcpp.h"
-
-
+#include "ObstacleAvoidanceConfig.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[])
+{
    int n = 3; // number of obstacles
-   float x = 0; float y = 0; // initial position of the object
-   float xg = 10; float yg = 10; // position of target
-   int i = 0; //starting index of object
+   float x = 0;
+   float y = 0; // initial position of the object
+   float xg = 10;
+   float yg = 10; // position of target
+   int i = 0;     //starting index of object
    float TotalAtt = 0;
    float TotalRep = 0;
    float TotalForce = 0;
@@ -21,13 +23,11 @@ int main() {
    {
       Forces obj;
       errors = obj.forceAtt(x, y, xg, yg, &out);
-      
+
       break;
-
-      
-
    }
 
-   cout << "the program works good ";
+   cout << "the program works good " << endl;
+   cout << argv[0] << " VERSION " << OBSTACLEAVOIDANCE_VERSION_MAJOR << "." << OBSTACLEAVOIDANCE_VERSION_MINOR << endl;
    return 0;
 }
